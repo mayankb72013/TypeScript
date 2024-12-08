5f443ab1f901a87fbc884c85e8be7e0173971ec4
Mayank Balmoori <balmoorimayank@gmail.com>
2024-12-08 23:12:57 +0530

    Understood the boolean type for the function

---
+++ prog3.js
@@ -0,0 +1,8 @@
+"use strict";
+function check(age) {
+    if (age >= 18) {
+        return true;
+    }
+    return false;
+}
+console.log(check(38));
---
+++ prog3.ts
@@ -0,0 +1,7 @@
+function check(age: number):boolean{
+    if(age>=18){
+        return true;
+    }
+    return false
+}
+console.log(check(38));
